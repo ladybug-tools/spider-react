9808adb57bb1fa67901a0f112f0a1531f7d0084c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 18:05:04 +0000

    Deploy ladybug-tools/spider-react to github.com/ladybug-tools/spider-react.git:gh-pages

---
+++ precache-manifest.d6cf38f84805c29473d9bb18bc77f2ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9509344d8fb5a08bfd65",
+    "url": "/spider-react/static/css/main.d6907b0f.chunk.css"
+  },
+  {
+    "revision": "9509344d8fb5a08bfd65",
+    "url": "/spider-react/static/js/main.6ab6cc14.chunk.js"
+  },
+  {
+    "revision": "8f62240ffebb261aeded",
+    "url": "/spider-react/static/js/runtime~main.74a4ac17.js"
+  },
+  {
+    "revision": "12c08dd608c1d806f9aa",
+    "url": "/spider-react/static/js/2.95315abc.chunk.js"
+  },
+  {
+    "revision": "5ff7758cc454d34139d2b94b4aaf10d6",
+    "url": "/spider-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c47b1d758e93c65b2e3a693f390a3dd.js"
+  "/spider-react/precache-manifest.d6cf38f84805c29473d9bb18bc77f2ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spider-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
